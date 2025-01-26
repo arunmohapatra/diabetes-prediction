b3c4938a5c527e1df2f32a409a427091489362b2
arunmohapatra <arun.mohapatra@yahoo.com>
2025-01-26 08:06:04 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,14 @@
-# This file makes the 'app' directory a package.
+"""
+This file makes the 'app' directory a package and sets up logging.
+"""
+
 import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+# Create a logger for the module
 logger = logging.getLogger(__name__)
